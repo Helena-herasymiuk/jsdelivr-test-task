71dfa71c3c8d8f1770726b22353f51b274bf14b7
heras.lena <herasymiuk@smarttender.biz>
2022-06-07 16:00:33 +0300

    init request

--- src/App.vue
+++ src/App.vue
@@ -2,53 +2,37 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
-      dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"/>
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"/>
-      </div>
-
-      <v-spacer/>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      color="deep-purple lighten-1">
+      <!-- <div
+        class="d-flex align-center justify-center "> -->
+      <packages-search/>
+      <!-- </div> -->
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <!-- 
+      :item-key="item_key" 
+     -->
+      <packages-table/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import PackagesTable from '@/components/PackagesTable'
+import PackagesSearch from '@/components/PackagesSearch'
 export default {
   name: 'App',
-
+  components: {
+    PackagesTable,
+    PackagesSearch
+  },
   data: () => ({
     //
   }),
-  created() {
-    console.log('fldjhfsvh')
-  }
 }
 </script>
+<style lang="scss">
+
+@import "~@/assets/scss/main";
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,78 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import PackagesApi from '@/api/packagesApi'
+const api = new PackagesApi()
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    packages: [],
+    packagesTotal: 0,
+    packagesSearchInfo: {
+      text: '',
+      size: 10, 
+      page: 1
+    },
+    loading: false
+    // packagesPage: 1
+
   },
   mutations: {
+    setPackages(state, payload) {
+      state.packages = Vue.prototype.$_.cloneDeep( payload.objects )
+      state.packagesTotal = Vue.prototype.$_.cloneDeep( payload.total )
+    },
+    setPackagesSearchInfo(state, payload) {
+      let obj = {}
+      if ( (payload.text || payload.size) && !payload.page) {
+        obj.page = 1
+      }
+      state.packagesSearchInfo = Object.assign(state.packagesSearchInfo, payload, obj)
+    },
+    startLoading(state) {
+      state.loading = true
+    },
+    finishLoading(state) {
+      state.loading = false
+    }
+    // setPerPage(state, payload) {
+    //   state.packagesPerPage = payload
+    // }
+  },
+  getters: {
+    getPackages(state) {
+      return state.packages
+    },
+    getPackagesTotal(state) {
+      return state.packagesTotal
+    },
+    getPackagesSearchInfo(state) {
+      return state.packagesSearchInfo
+    },
+    getLoading(state) {
+      return state.loading
+    }
+    // getPage(state) {
+    //   return state.packagesPage
+    // },
   },
   actions: {
+    async searchPackages({commit, getters, state}, payload) {
+      commit('setPackagesSearchInfo', payload)
+      commit('startLoading')
+
+      let info = getters.getPackagesSearchInfo,
+        from = (info.page - 1) * info.size
+      console.log( info, 'searchPackages', state)
+      return await api.searchPackages({...info, from})
+        .then(response => {
+          commit('setPackages', response)
+          
+        })
+        .finally(() => commit('finishLoading') )
+    }
   },
   modules: {
   },
-});
+})
