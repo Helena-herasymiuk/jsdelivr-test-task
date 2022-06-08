753530f3124ea42a516e73a475871c3e8a8e1b70
heras.lena <herasymiuk@smarttender.biz>
2022-06-08 12:01:48 +0300

    Initial dist subtree commit

---
+++ precache-manifest.aed43850cb6c134e9d8eb2fe91ae9d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91f1aa3b6f6f27a990d",
+    "url": "/jsdelivr-test-task/css/app.3bc98417.css"
+  },
+  {
+    "revision": "718f4c141d857dee4f50",
+    "url": "/jsdelivr-test-task/css/chunk-vendors.b6c21d80.css"
+  },
+  {
+    "revision": "b2a37a6a88973e2775301b5ceae8dec4",
+    "url": "/jsdelivr-test-task/index.html"
+  },
+  {
+    "revision": "c91f1aa3b6f6f27a990d",
+    "url": "/jsdelivr-test-task/js/app.aaf05399.js"
+  },
+  {
+    "revision": "718f4c141d857dee4f50",
+    "url": "/jsdelivr-test-task/js/chunk-vendors.6e31167e.js"
+  },
+  {
+    "revision": "55b5d63a6bb426a67facb2b6fccbde67",
+    "url": "/jsdelivr-test-task/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jsdelivr-test-task/robots.txt"
+  }
+]);
